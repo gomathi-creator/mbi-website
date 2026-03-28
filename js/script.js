08ad5c04278e4e6a55f908a81ef67f0993f2e746
gomathi-creator <gomathibg552@gmail.com>
2026-03-28 12:56:01 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ hamburger.addEventListener("click", () => {
     console.log("CLICKED");
     navMenu.classList.toggle("active");
 });
+
+const philosophy = document.querySelector(".philosophy-text");
+
+window.addEventListener("scroll", () => {
+    const triggerPoint = window.innerHeight * 0.85;
+    const sectionTop = philosophy.getBoundingClientRect().top;
+
+    if (sectionTop < triggerPoint) {
+        philosophy.classList.add("show");
+    }
+});
