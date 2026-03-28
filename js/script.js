d58d28ad974f91a56a11b32b736b4d436a30bfde
gomathi-creator <gomathibg552@gmail.com>
2026-03-28 12:43:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+console.log("JS LOADED");
+
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navMenu");
 
 hamburger.addEventListener("click", () => {
+    console.log("CLICKED");
     navMenu.classList.toggle("active");
 });
-console.log("JS connected");
